83f92816b79669faeb1395556b7594e4f604c59b
Hewr <hewrtarkhany@gmail.com>
2022-04-27 08:12:17 -0700

    getting about page

--- wordle-web/layouts/default.vue
+++ wordle-web/layouts/default.vue
@@ -2,11 +2,43 @@
   <v-app>
     <v-app-bar fixed app>
       <router-link to="/" class="text-h4 font-weight-black no-decoration">
-        Wordle
+        <v-icon> mdi-gamepad-variant </v-icon> Burbank & Tarkhany Wordle
       </router-link>
       <v-spacer />
       <settings-dialog />
+      
+      
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-btn icon>
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
     </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      right
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="white--text text--accent-4"
+        >
+        <router-link to="/components/about" class="text-h4 font-weight-black no-decoration">
+          <v-list-item>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
+        </router-link>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+
     <v-main>
       <v-container>
         <Nuxt />
@@ -18,13 +50,24 @@
   </v-app>
 </template>
 
-<script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
-import SettingsDialog from '@/components/settings-dialog.vue'
 
-@Component({ components: { SettingsDialog } })
-export default class DefaultLayout extends Vue {}
+
+<script lang="ts">
+//import  About  from '~/components/about'
+export default {
+    data: () => ({
+      drawer: false,
+      group: null,
+    }),
+    watch: {
+      group () {
+        let drawer = false
+      },
+    },
+    to:'/about',
+  }
 </script>
+
 
 <style>
 .no-decoration {
