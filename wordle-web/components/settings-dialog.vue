e7a26a7a78be0630ca06508626c027d2d00fb61c
Hewr <hewrtarkhany@gmail.com>
2022-04-26 21:40:48 -0700

    setting dialog

--- wordle-web/components/settings-dialog.vue
+++ wordle-web/components/settings-dialog.vue
@@ -39,6 +39,12 @@
                   <v-list-item @click="purpleTheme">
                     <v-list-item-title> Purple </v-list-item-title>
                   </v-list-item>
+                  <v-list-item @click="fallTheme">
+                    <v-list-item-title> Fall </v-list-item-title>
+                  </v-list-item>
+                  <v-list-item @click="graphiteTheme">
+                    <v-list-item-title> Graphite </v-list-item-title>
+                  </v-list-item>
                 </v-list-item-group>
               </v-list>
             </v-menu>
@@ -52,23 +58,18 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import { colors } from 'vuetify/lib'
-
 @Component({})
 export default class SettingsDialog extends Vue {
   dialog = false
-
   toggleDialog() {
     this.dialog = !this.dialog
   }
-
   turnOnTheLights() {
     this.$vuetify.theme.dark = false
   }
-
-  turnOffTheLights() {
+  turnOffTheLights() { 
     this.$vuetify.theme.dark = true
   }
-
   purpleTheme() {
     const purpleTheme = {
       primary: colors.deepPurple,
@@ -79,9 +80,34 @@ export default class SettingsDialog extends Vue {
       error: colors.pink.accent3,
       success: colors.deepPurple.lighten4,
     }
-
     this.$vuetify.theme.themes.dark = purpleTheme
     this.$vuetify.theme.themes.light = purpleTheme
+  }
+  fallTheme() {
+    const fallTheme = {
+      primary: colors.brown,
+      accent: colors.deepOrange.accent3,
+      secondary: colors.deepOrange,
+      info: '#FFEB3B',
+      warning: colors.orange.accent1,
+      error: colors.orange.accent3,
+      success: colors.brown.lighten4,
+    }
+    this.$vuetify.theme.themes.dark = fallTheme
+    this.$vuetify.theme.themes.light = fallTheme
+  }
+  graphiteTheme() {
+    const graphiteTheme = {
+      primary: colors.grey.darken3,
+      accent: colors.grey.lighten3,
+      secondary: colors.grey,
+      info: '#78909C',
+      warning: colors.blueGrey.lighten3,
+      error: colors.blueGrey.lighten1,
+      success: colors.grey.lighten1,
+    }
+    this.$vuetify.theme.themes.dark = graphiteTheme
+    this.$vuetify.theme.themes.light = graphiteTheme
   }
 }
 </script>
