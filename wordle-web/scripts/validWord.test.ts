ec9b3c6f2e5aa1a30ebff4604d0d532ba3442be8
Hewr <hewrtarkhany@gmail.com>
2022-05-02 23:06:59 -0700

    chaning the test

--- wordle-web/scripts/validWord.test.ts
+++ wordle-web/scripts/validWord.test.ts
@@ -2,23 +2,21 @@ import {ValidWord} from "./valid"
 
 describe('Get Valid Words', () => {
     test('get one word if word is valid', () => {
-      const word = "acorn"; const num=0;
-        const validWords= ValidWord.givehints(word,num);
+      const word = "acorn"; 
+        const validWords= ValidWord.givehints(word);
 
         expect(validWords.length).toBe(1);
 
         expect(validWords[0]).toBe(word)
-        expect(validWords.length=num).toBe(3)
 
 
    
     })
     test('get one work if it ?', () => {
-      const word = "ac???"; const num =0;
-        const validWords= ValidWord.givehints(word, num);
+      const word = "ac???"; 
+        const validWords= ValidWord.givehints(word);
 
         expect(validWords.length).toBe(3);
-        expect(validWords.length==num).toBe(3);
 
    
     })
