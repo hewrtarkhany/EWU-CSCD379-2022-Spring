5b7413748bc2dd6de25e1301ac837dbf972ed9d5
Hewr <hewrtarkhany@gmail.com>
2022-05-26 11:30:26 -0700

    adding instruction page not completed

---
+++ wordle-web/pages/daily.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container fluid fill-height justify-center>
+    <v-card>
+      <v-card-title class="display-3 justify-center">
+        Leader's Stats
+      </v-card-title>
+      <v-container v-for="apiData in dayStat" :key="apiData">
+        <day-stats :today="apiData"></day-stats>
+      </v-container>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+@Component({})
+export default class DayData extends Vue {
+  dayStat :any = []
+  created() {
+  this.getData()
+  }
+  async getData() {
+    const response=await this.$axios.get('/api/Recents');
+    this.dayStat=response.data;
+  }
+}
+</script>
