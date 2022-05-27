06cf8be68deb36dfd6298db61e891b8408428215
Hewr <hewrtarkhany@gmail.com>
2022-05-27 13:01:57 -0700

    changing the daily stats to get a promise

--- wordle-web/pages/daily.vue
+++ wordle-web/pages/daily.vue
@@ -4,24 +4,62 @@
       <v-card-title class="display-3 justify-center">
         Leader's Stats
       </v-card-title>
-      <v-container v-for="apiData in dayStat" :key="apiData">
-        <day-stats :today="apiData"></day-stats>
-      </v-container>
+      <v-card-text class="text-center">
+          {{names}}
+      </v-card-text>
+      <v-card-text>
+        <v-simple-table>
+            <thead>
+              <tr>
+                <th> Date </th>
+                <th style="text-align:center"> Score </th>
+                <th style="text-align:center"> Time</th>
+                <th style="text-align:center"> Games </th>
+                <th style="text-align:center"> Played </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="( dayStat,date) in  dayStats" 
+              :key="date"
+              >
+                <td> {{ dayStat.date}}</td>
+
+                <td style="text-align: center" >{{dayStat.score}} </td>
+                <td style="text-align: center" >{{dayStat.time}} </td>
+                <td style="text-align: center" >{{dayStat.games}} </td>
+                <td style="text-align: center" >{{dayStat.played}} </td>
+
+              </tr>
+            </tbody>
+        </v-simple-table>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="green" @click="getData">
+          10 Words
+        </v-btn>
+      </v-card-actions>
     </v-card>
+
   </v-container>
 </template>
 
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator'
+
 @Component({})
 export default class DayData extends Vue {
-  dayStat :any = []
+  dayStats :any = []
+  names :string=''
+
   created() {
   this.getData()
   }
-  async getData() {
-    const response=await this.$axios.get('/api/Recents');
-    this.dayStat=response.data;
+  // creating a response data with player guid number to get the right player
+  getData() {
+    this.names= '10 Daily Words'
+    this.$axios.get('/api/DateWord').then((reponse)=>{
+      this.dayStats = reponse.data
+    })
   }
 }
 </script>
