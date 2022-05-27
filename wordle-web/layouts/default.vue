d360be8490f8a8995535e836d10ee922cad997ed
Hewr <hewrtarkhany@gmail.com>
2022-05-27 13:44:44 -0700

    adding daily status to the main page

--- wordle-web/layouts/default.vue
+++ wordle-web/layouts/default.vue
@@ -31,6 +31,11 @@
             Play <v-icon>mdi-controller-classic</v-icon></v-btn
           >
         </v-list-item>
+         <v-list-item>
+          <v-btn text block nuxt to="/dailyStats">
+            Daily Stats<v-icon>mdi-controller-classic</v-icon></v-btn
+          >
+        </v-list-item>
 
         <v-list-item>
           <v-btn text block nuxt to="/about">
