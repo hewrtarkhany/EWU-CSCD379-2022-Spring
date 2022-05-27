dfbb2ed31b2fa456e8abec7b117994881efa4bd1
Hewr <hewrtarkhany@gmail.com>
2022-05-27 11:07:50 -0700

    day stats

---
+++ wordle-web/components/day-data.vue
@@ -0,0 +1,8 @@
+<script lang="ts">
+import { Component,Prop,Vue } from "vue-property-decorator";
+@Component ({components:{}})
+export default class DayData extends Vue{
+    @Prop({required: true})
+    stats =null;
+}
+</script>
