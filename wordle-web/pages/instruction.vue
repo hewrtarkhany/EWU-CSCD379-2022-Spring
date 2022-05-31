95b4bfb06e754693c0bda75aab2b296dace951f3
Hewr <hewrtarkhany@gmail.com>
2022-05-31 12:29:38 -0700

    instruction page is almost done

--- wordle-web/pages/instruction.vue
+++ wordle-web/pages/instruction.vue
@@ -13,9 +13,27 @@
         <v-card>
           <div class="d-flex align-center">
             <v-card-text decoration>
-              info about the game
-              <v-spacer />
-              <v-card-text> more isntruction </v-card-text>
+              <ul>
+                <li>
+                  the game is based on choosing the right word
+                </li>
+                <li>
+                 once you start the game, you get to right letters on the keyboard, and guess it.
+                </li>
+                <li>
+                  the guess button will give you hints and below that, you get to see the words are available.
+                </li>
+                <li>
+                  you will only have 6 tries to get the guess right.
+                </li>
+                <li>
+                  if you get the guess right then your name will go on leader board
+                </li>
+                <li>
+                  then you can review the stats based on plays 
+                </li>
+
+              </ul>
             </v-card-text>
           </div>
         </v-card>
@@ -26,6 +44,7 @@
           <v-card-title class="justify-center">
             <h1>Instructions</h1>
           </v-card-title>
+
         </v-card>
       </v-col>
 
@@ -34,13 +53,20 @@
           <div class="d-flex align-center">
             <v-container fluid >
               <v-card flat>
-                <v-card-title class="justify-left"> title </v-card-title>
+                <v-card-title class="justify-left"> Step by Step </v-card-title>
 
                 <v-card-text>
-                  some stuff
-                  <v-spacer />
-                </v-card-text>
+                  <ui>
+                    <li>
+                      in the right side choose the Play from( home, play, daily stats, instruction,leaderboard, settings)
+                    </li>
+                    <li>
 
+                    </li>
+                  </ui>
+
+
+                </v-card-text>
                 <v-card-actions>
                   <v-btn button nuxt to="/game" color="primary"> Play </v-btn>
                 </v-card-actions>
@@ -53,7 +79,7 @@
   </v-container>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue'
 import Component from 'vue-class-component'
 
