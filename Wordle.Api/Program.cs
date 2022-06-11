dc2f6730a950321d1b6b49faee554b225d7eb94f
Tyler Rose <trose8@ewu.edu>
2022-06-11 15:43:23 -0700

    Connection String

--- Wordle.Api/Program.cs
+++ Wordle.Api/Program.cs
@@ -62,7 +62,7 @@ builder.Services.AddSwaggerGen(config =>
     });
 builder.Services.AddScoped<ILeaderBoardService, LeaderBoardServiceMemory>();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("AzureConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddScoped<ScoreStatsService>();
 builder.Services.AddScoped<PlayersService>();
